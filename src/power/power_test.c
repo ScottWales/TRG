644b4b0982112929fd2aaa70211bde86fcb9fb52
Scott Wales <wales@physics.usyd.edu.au>
2009-12-03 15:39:00 +1100

    Added struct to hold power

---
+++ src/power/power_test.c
@@ -0,0 +1,65 @@
+/** 
+ * @file power_test.c
+ * @brief 
+ *
+ * Copyright (C) 2009 Scott Wales
+ *
+ * @author Scott Wales <scottwales@coater.physics.usyd.edu.au>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "power.h"
+
+#include "cmockery_extra.h"
+
+#include "power.c"
+
+struct Cosmos * CosmosCreate(double a)
+{
+  return malloc(1);
+}
+
+void CosmosDestroy(struct Cosmos * c)
+{
+  free(c);
+}
+
+void test_power_init(void ** status)
+{
+  struct Power * power = PowerCreate(0.1);
+  PowerDestroy(power);
+}
+
+void power_setup(void ** state)
+{ 
+  struct Power * power = PowerCreate(0.1);
+  *state = power;
+}
+
+void power_teardown(void ** state)
+{
+  struct Power * power = *state;
+  PowerDestroy(power);
+}
+
+int main(int argc, char ** argv)
+{
+  struct UnitTest tests[] = {
+    unit_test(test_power_init),
+  };
+  return run_tests(tests);
+}
