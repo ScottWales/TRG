25639ccd95f19ff687909d375e8474262fd464fa
Scott Wales <wales@physics.usyd.edu.au>
2009-12-03 15:32:39 +1100

    Filled out main stub

--- src/trg.c
+++ src/trg.c
@@ -22,8 +22,30 @@
  * Boston, MA 02110-1301, USA.
  */
 
+#include <math.h>
+#include <stddef.h>
+
+#include "solver/solver.h"
+
 int main(int argc, char ** argv)
 {
+  double a = 0.01;
+  size_t nsteps = 100;
+
+  struct Solver * solver = SolverCreate(a);
+  
+  double logastep = (1-log(a))/(double)nsteps;
+  for (double loga = log(a); loga <= 1; loga += logastep){
+    double newa = exp(loga);
+    SolverStep(solver,newa);
+    SolverDump(solver);
+  }
+  double newa = 1.;
+  SolverStep(solver,newa);
+  SolverDump(solver);
+
+  SolverDestroy(solver);
+
   return 0;
 }
 
